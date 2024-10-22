8722636332f03c6fc3160949e7d4659362eea87a
madhavi569 <madhavireddi27@gmail.com>
2024-10-22 11:02:26 +0530

    Create Calculate.py

---
+++ Calculate.py
@@ -0,0 +1,12 @@
+'''
+ Write a  Python Program to calculate the number of words and characters present in a string.
+Input & Output Format:
+Input consists of a string.
+Output consists of two integers.
+First output refers to the count of the words.
+Second output refers to the count of the characters in a given string,
+Sample Input:
+Cyfuno
+'''
+Words: 1
+Letters: 6
