4b47643bfbc5e2fd858b78c28eadc330beabfa1b
madhavi569 <madhavireddi27@gmail.com>
2024-10-22 11:06:51 +0530

    Create Index.py

---
+++ Index.py
@@ -0,0 +1,13 @@
+'''
+Write a Python Program to remove the nth index character from a non-empty string.
+Input & Output Format:
+Input consists of a string and one integer.
+The first input consists of a string.
+The second input refers to the index position.
+The output consists of a string.
+Sample Input:
+cyfuno
+4
+'''
+cyfuo
+
